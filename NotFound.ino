7bfc7d2136651bc07b53a60892441c992661ed68
rvangelder11 <90907092+rvangelder11@users.noreply.github.com>
2023-07-31 13:46:58 +0200

    Delete NotFound.ino

--- NotFound.ino
+++
@@ -1,5 +0,0 @@
-//-----------------------------------
-// This sends back a 404 message in case of an error
-void notFound(AsyncWebServerRequest * request) {                // error handling
-  request->send(404, "text/plain", "Not found");
-}
